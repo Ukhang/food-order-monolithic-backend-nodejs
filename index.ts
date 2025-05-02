01ba1c9ffe1c854325e6c303e3489edaf6587c0c
Ukhang <ukhangmarma459@gmail.com>
2025-05-02 19:06:56 +0600

    optimize code separate and clean up server start and db connection functions

--- index.ts
+++ index.ts
@@ -1,25 +1,17 @@
 import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import App from "./services/ExpressApp";
+import dbConnection from "./services/Database";
 
-import { AdminRoute, VendorRoute } from "./routes";
-import { MONGO_URI } from "./config";
-import path from "path";
+const startServer = async () => {
+  const app = express();
 
-const app = express();
+  await dbConnection();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/images', express.static(path.join(__dirname, 'images')));
+  await App(app);
 
-app.use("/admin", AdminRoute);
-app.use("/vendor", VendorRoute);
+  app.listen(8000, () => {
+    console.log("Listing to the port: 8000")
+  });
+};
 
-mongoose.connect(MONGO_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Failed to connect to MongoDB", err));
-
-app.listen(8000, () => {
-    console.clear();
-    console.log("app is listing to the port: 8000");
-})
+startServer();
