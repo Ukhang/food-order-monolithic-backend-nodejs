3bbd506ff44c94ee25adf6cff0a7f37bef661f2e
Ukhang <ukhangmarma459@gmail.com>
2025-05-08 21:34:42 +0600

    add GetTopRestaurants controller

--- controllers/ShoppingController.ts
+++ controllers/ShoppingController.ts
@@ -21,6 +21,22 @@ export const GetFoodAvailability =  async (req: Request, res: Response, next: Ne
 
 export const GetTopRestaurants = async (req: Request, res: Response, next: NextFunction) => {
 
+    const pincode = req.params.pincode;
+    
+    const result = await Vendor.find({
+        pincode: pincode,
+        serviceAvailable: false
+    })
+    .sort([['rating', 'descending']])
+    .limit(10)
+    
+    if (result.length > 0) {
+        res.status(200).json(result);
+        return;
+    };
+
+    res.status(400).json({ message: "Data Not Found!" });
+
 };
 
 export const GetFoodsIn30Min = async (req: Request, res: Response, next: NextFunction) => {
